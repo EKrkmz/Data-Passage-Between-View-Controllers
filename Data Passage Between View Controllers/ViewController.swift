3fac59bf483d28c24838da6bb7fa370060f33b02
Elif Korkmaz <korkmazelif7797@gmail.com>
2021-07-05 11:43:58 +0300

    Initial Commit

---
+++ Data Passage Between View Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Data Passage Between View Controllers
+//
+//  Created by Elif Korkmaz on 5.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
